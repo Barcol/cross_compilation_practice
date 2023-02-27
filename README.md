c24402242f6457978a31e2101061d3649d03a2ea
Radoslaw Bigaj <radoslaw.bigaj@brandwatch.com>
2023-02-27 17:01:47 +0100

    Add random dependencies

--- README.md
+++ README.md
@@ -1,5 +1,10 @@
 # cross_compilation_practice
-## Hello Tokio
+
+### UUID and SysInfo
+Build with same command. Does not produce any errors but temperature info is missing on vacuum.
+
+### Hello Tokio
 Build wih `CARGO_TARGET_ARMV7_UNKNOWN_LINUX_MUSLEABIHF_LINKER=rust-lld cargo build --release --target armv7-unknown-linux-musleabihf`
+
 Works on Vacuum flawlessly.
 
--- src/main.rs
+++ src/main.rs
@@ -12,4 +12,23 @@ async fn main() {
 
     // Calling `.await` on `op` starts executing `say_world`.
     op.await;
+
+
+     use uuid::Uuid;
+ 
+     let id = Uuid::new_v4();
+     println!("{}", id);
+
+     use sysinfo::{System, SystemExt};
+
+let mut sys = System::new_all();
+
+sys.refresh_all();
+
+// Components temperature:
+println!("=> components:");
+for component in sys.components() {
+    println!("{:?}", component);
+}
+
 }
